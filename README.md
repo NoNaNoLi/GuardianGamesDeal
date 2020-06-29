e1548e2180b471b8c8fa71745d15bebe3c00e2f7
ArikusuF <qqshuyy@gmail.com>
2020-06-30 00:42:02 +0800

    Update

--- README.md
+++ README.md
@@ -1,60 +1,44 @@
-# 简单的招聘网站示例
-基于 Flask / Jinja2 / Bootstrap / MySQL 开发，仿照拉勾网的风格，实现了招聘网站的必需功能
+# Guardian Game Deals
 
-## 环境
-* Python 3
-* MySQL
+###### An aggregate game deal Info Site
 
-## 快速开始
+Developed with Flask / Jinja2 / Bootstrap / MySQL
 
-#### 1. 安装 Python 依赖
-```sh
+### Environment 
+
++ Pyton 3.8
++ Flask
++ MySQL
+
+### Deployment
+
+1. Install Python dependecies
+
+```bash
 $ pip3 install -r requirements.txt
 ```
 
-#### 2. 修改配置文件
+2. Edit Config file
 
-根据自己情况，修改 `job_web/config.py`
 
-主要是 `SQLALCHEMY_DATABASE_URI` 数据库的链接
 
-#### 3. 创建数据库
+​		Note: The scraper function (@Qicheng Wang) requires an open 443 port. Certain hosts (e.g. Pythonanywhere Free Account) cannot support its deployment.
 
-根据上面配置中的库名，创建数据库
+2. Build database
 
-#### 4. 利用 flask-migrate 建表
+### Achieved Function
 
-命令行终端，先进入项目目录，然后依次执行下列命令：
++ Dynamic price updates on top bestsellers from Steam
++ Simple login
++ Game Page
++ Store Page
 
-```sh
-$ export FLASK_APP=manage.py
-# windows 系统：set FLASK_APP=manage.py
+### TODO
 
-$ flask db init
-$ flask db migrate
-$ flask db upgrade
-```
+- [ ] Login with auth
+- [ ] Allow waterfall flow on the front page
+- [ ] Better data structure for DLC affiliation
+- [ ] Global Search Function
 
-#### 5. 生成测试数据（可选）
+### Demo
 
-可执行 [test_data.py](https://github.com/zkqiang/job-web-demo/blob/master/data/test_data.py) 生成一些随机数据
-
-## 实现功能
-* 个人和企业两种角色的注册登录编辑
-* 职位和企业的索引页、详情页及搜索功能
-* 个人简历上传和投递操作
-* 企业对职位的增删改查上下线，及对简历的反馈处理
-
-## TODO
-- [ ] 职位和企业的条件筛选
-- [ ] 管理员后台和权限功能
-- [ ] 简历支持 PDF，并将 PDF 转图片在线浏览
-- [ ] 职位和企业该为列表展示
-- [ ] 个人对职位收藏
-
-## 运行截图
-![pic](docs/1.png '首页')
-![pic](docs/2.png '职位详情')
-![pic](docs/3.png '企业详情-在招岗位')
-![pic](docs/4.png '企业管理-职位管理')
-![pic](docs/5.png '个人管理-求职记录')
---
+++ app/flask_app.py
@@ -0,0 +1,141 @@
+from flask import Flask, render_template
+import requests
+from pyquery import PyQuery as pq
+import pymysql
+
+app = Flask(__name__)
+
+# database initialisation
+dbHost = "localhost"
+dbUser = "root"
+dbPass = "233233"
+dbase = "test"
+dbCharset = 'utf8'
+
+gName = {}
+gDiscount = {}
+gdate = {}
+gprice = {}
+
+def create():
+    db = pymysql.connect(host=dbHost,
+                         user=dbUser,
+                         passwd=dbPass,
+                         db=dbase,
+                         charset=dbCharset)
+
+    cursor = db.cursor()
+    cursor.execute("DROP TABLE IF EXISTS STEAM_DISCOUNT")
+
+    sql = """CREATE TABLE STEAM_DISCOUNT(
+            ID INT PRIMARY KEY AUTO_INCREMENT,
+            NAME CHAR(100),
+            DISCOUNT CHAR(100),
+            PUBLISH_DATE CHAR(100),
+            ORIGINAL_PRICE CHAR(100))"""
+
+    cursor.execute(sql)
+    db.close()
+
+
+def insert(data):
+    db = pymysql.connect(dbHost, dbUser, dbPass, dbase, charset=dbCharset)
+    cursor = db.cursor()
+    for i in data:
+        game = i
+        sql = "INSERT INTO STEAM_DISCOUNT(NAME,DISCOUNT,PUBLISH_DATE,ORIGINAL_PRICE) values(%s,%s,%s,%s)"
+        cursor.execute(sql, game)  # 执行sql语句，movie即是指要插入数据库的数据
+        db.commit()  # 插入完成后，不要忘记提交操作
+        #print("success")
+    cursor.close()
+    db.close()
+
+
+def show():
+    db = db = pymysql.connect(dbHost, dbUser, dbPass, dbase, charset=dbCharset)
+    cursor = db.cursor()
+    sql = "select * from STEAM_DISCOUNT"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    db.commit()
+    cursor.close()
+    db.close()
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route("/")
+def hello():
+    return render_template(
+        'index.html',
+        # game 1 information
+        gName1=gName[1],
+        gDiscount1=gDiscount[1],
+        gdate1=gdate[1],
+        gprice1=gprice[1],
+        gStore1="steam",
+    
+        # game 2 information
+        gName2=gName[2],
+        gDiscount2=gDiscount[2],
+        gdate2=gdate[2],
+        gprice2=gprice[2],
+        gStore2="steam",
+    
+        # game 3 information
+        gName3=gName[3],
+        gDiscount3=gDiscount[3],
+        gdate3=gdate[3],
+        gprice3=gprice[3],
+        gStore3="steam",
+    
+        # game 4 information
+        gName4=gName[4],
+        gDiscount4=gDiscount[4],
+        gdate4=gdate[4],
+        gprice4=gprice[4],
+        gStore4="steam",
+    
+        # game 5 information
+        gName5=gName[5],
+        gDiscount5=gDiscount[5],
+        gdate5=gdate[5],
+        gprice5=gprice[5],
+        gStore5="steam",
+    
+        # game 6 information
+        gName6=gName[6],
+        gDiscount6=gDiscount[6],
+        gdate6=gdate[6],
+        gprice6=gprice[6],
+        gStore6="steam"
+    )
+
+
+if __name__ == "__main__":
+    create()
+
+    list_data = []
+    url = 'https://store.steampowered.com/search/?filter=globaltopsellers'
+    for i in range(1):
+        newurl = url + str(i)
+        print(newurl)
+        html = requests.get(newurl).content.decode('utf-8')
+        doc = pq(html)
+        items = doc('#search_result_container a').items()
+        cnt = 0
+        for item in items:
+            gName[cnt] = item.find('.title').text()
+            gDiscount[cnt] = item.find('.search_discount.responsive_secondrow').text()
+            gdate[cnt] = item.find('.search_released.responsive_secondrow').text()
+            gprice[cnt] = item.find('.search_price.discounted.responsive_secondrow span').text()
+            if gDiscount[cnt] == "" or gprice[cnt] == "":
+                pass
+            else:
+                list_data.append([gName[cnt], gDiscount[cnt], gdate[cnt], gprice[cnt]])
+                # print(gName[cnt], gDiscount[cnt], gdate[cnt], gprice[cnt])
+            cnt = cnt + 1
+    insert(list_data)
+    app.run(debug=True)
